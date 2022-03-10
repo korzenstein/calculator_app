044d50c7278b526f3640a09413e708a2f36d1caf
Stephen Korzenstein <skorzenstein@gmail.com>
2022-03-09 23:21:36 -0500

    added draft toggle functionality and css styles

--- main.js
+++ main.js
@@ -1,27 +1,58 @@
-let display = document.getElementById('display');
-let buttons = Array.from(document.getElementsByClassName('button'))
-// Functionality
+let display = document.getElementById("display");
+let buttons = Array.from(document.getElementsByClassName("button"));
 
-buttons.map( button => {
-    button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
-            case 'RESET':
-                display.innerText = '';
-                break;
-            case '=':
-                try{
-                    display.innerText = Function('"use strict"; display.innerText')();
-                } catch {
-                    display.innerText = "Error"
-                }
-                break;
-            case 'DEL':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-            default:
-                display.innerText += e.target.innerText;
+let radios = Array.from(document.getElementsByClassName("radio"));
+let buttonGroup = document.querySelector(".buttonGroup");
+
+// Toggle Themes Functionality
+
+(() => {
+  purple()
+})();
+
+function purple() {
+    buttonGroup.classList.remove("orange", "black");
+    buttonGroup.classList.add("purple");
+}
+
+function black() {
+    buttonGroup.classList.remove("purple", "orange");
+    buttonGroup.classList.add("black");
+}
+
+function orange() {
+    buttonGroup.classList.remove("purple", "black");
+    buttonGroup.classList.add("orange");
+}
+
+// radios.forEach((radio) => {
+//     radio.addEventListener("click", () => {
+//         return(radio.id === 1 ? buttonGroup.classList.add("purple") : console.log(radio.id))
+//     })
+// })
+
+// Calc Functionality
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "RESET":
+        display.innerText = "";
+        break;
+      case "=":
+        try {
+          display.innerText = Function('"use strict"; display.innerText')();
+        } catch {
+          display.innerText = "Error";
         }
-    });
+        break;
+      case "DEL":
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
+        }
+        break;
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
 });
