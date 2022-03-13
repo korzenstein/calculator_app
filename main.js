1b56f4162710889204dd3e7702869db23ba322b3
Stephen Korzenstein <skorzenstein@gmail.com>
2022-03-13 19:55:45 -0400

    modified javascript for theme toggle feature and added styles to partials

--- main.js
+++ main.js
@@ -5,42 +5,112 @@ let buttons = Array.from(document.getElementsByClassName("button"));
 let main = document.querySelector(".main")
 let output = document.querySelector(".output")
 let buttonGroup = document.querySelector(".buttonGroup");
-let butt = document.querySelector(".button");
-let equals = document.getElementById("equals");
+let primaryButtons = document.querySelectorAll(".primaryButton");
+let secondaryButtons = document.querySelectorAll(".secondaryButton")
+let equals = document.querySelector(".equalsButton");
+// let title = document.querySelector(".title");
+// let subtitle = document.querySelector(".subtitle");
+let toggleLables = document.querySelector(".toggleLabels")
+let backgrounds = document.querySelectorAll(".backgrounds")
+let textA = document.querySelectorAll(".textA")
 
 
+let allClasses = [main, output, buttonGroup, primaryButtons, secondaryButtons, equals, textA];
 // Toggle Themes Functionality
+
+function themeOne() {
+    // Remove Theme Two
+    main.classList.remove("twoBgGray")
+    output.classList.remove("twoBgLightGray")
+    buttonGroup.classList.remove("twoBgRedish")
+    primaryButtons.forEach(pb => pb.classList.remove("twoKeysLtYellow", "twoTextDarkGr", "twoKeysDarkGrayOrg"))
+    secondaryButtons.forEach(sb => sb.classList.remove("twoKeysModCyan", "twoTextWhite" , "twoKeysDarkCyan"))
+    equals.classList.remove("twoKeysOrange", "twoKeysDarkOrg", "twoTextWhite")
+    textA.forEach(text => text.classList.remove("twoTextDarkGr"))
+    // Remove Theme Three
+    main.classList.remove("threeVioletMain");
+    output.classList.remove("threeVioletToggle")
+    buttonGroup.classList.remove("threeVioletToggle")
+    primaryButtons.forEach(pb => pb.classList.remove("threeDarkVioletKey", "threeTextYel", "threeMagentaShadow"));
+    secondaryButtons.forEach(sb => sb.classList.remove("threeVioletKey", "threeTextWhite" , "threeMagentaShadow"));
+    equals.classList.remove("threeCyanKey", "threeCyanShadow", "threeTextBlue")
+    textA.forEach(text => text.classList.remove("threeTextYel"))
+
+    // THEME ONE:
+    // Add New Theme
+    main.classList.add("oneBg");
+    output.classList.add("oneScreen")
+    buttonGroup.classList.add("oneScreen")
+        // group classes
+    primaryButtons.forEach(pb => pb.classList.add("oneKeyGrOrange", "oneTextGrBlue", "oneKeyShadowOrange"))
+    secondaryButtons.forEach(sb => sb.classList.add("oneKeyBgBlue", "oneTextWhite" , "oneKeyShadowBlue"))
+    equals.classList.add("oneKeyBgRed", "oneKeyShadowRed", "oneTextWhite")
+    textA.forEach(text => text.classList.add("oneTextWhite"))
+}
+
+
+function themeTwo() {
+      // Remove Theme One
+    main.classList.remove("oneBg");
+    output.classList.remove("oneScreen")
+    buttonGroup.classList.remove("oneScreen")
+    primaryButtons.forEach(pb => pb.classList.remove("oneKeyGrOrange", "oneTextGrBlue", "oneKeyShadowOrange"))
+    secondaryButtons.forEach(sb => sb.classList.remove("oneKeyBgBlue", "oneTextWhite" , "oneKeyShadowBlue"))
+    equals.classList.remove("oneKeyBgRed", "oneKeyShadowRed", "oneTextWhite")
+    textA.forEach(text => text.classList.remove("oneTextWhite"))
+ // Remove Theme Three
+    main.classList.remove("threeVioletMain");
+    output.classList.remove("threeVioletToggle")
+    buttonGroup.classList.remove("threeVioletToggle")
+    primaryButtons.forEach(pb => pb.classList.remove("threeDarkVioletKey", "threeTextYel", "threeMagentaShadow"));
+    secondaryButtons.forEach(sb => sb.classList.remove("threeVioletKey", "threeTextWhite" , "threeMagentaShadow"));
+    equals.classList.remove("threeCyanKey", "threeCyanShadow", "threeTextBlue")
+    textA.forEach(text => text.classList.remove("threeTextYel"))
+
+    // THEME TWO: 
+    // Add New Theme
+    main.classList.add("twoBgGray")
+    output.classList.add("twoBgLightGray")
+    buttonGroup.classList.add("twoBgRedish")
+        // group classes
+    primaryButtons.forEach(pb => pb.classList.add("twoKeysLtYellow", "twoTextDarkGr", "twoKeysDarkGrayOrg"))
+    secondaryButtons.forEach(sb => sb.classList.add("twoKeysModCyan", "twoTextWhite" , "twoKeysDarkCyan"))
+    equals.classList.add("twoKeysOrange", "twoKeysDarkOrg", "twoTextWhite")
+    textA.forEach(text => text.classList.add("twoTextDarkGr"))
+}
+
+function themeThree() {
+    // Remove Theme One
+    main.classList.remove("oneBg");
+    output.classList.remove("oneScreen")
+    buttonGroup.classList.remove("oneScreen")
+    primaryButtons.forEach(pb => pb.classList.remove("oneKeyGrOrange", "oneTextGrBlue", "oneKeyShadowOrange"))
+    secondaryButtons.forEach(sb => sb.classList.remove("oneKeyBgBlue", "oneTextWhite" , "oneKeyShadowBlue"))
+    equals.classList.remove("oneKeyBgRed", "oneKeyShadowRed", "oneTextWhite")
+    textA.forEach(text => text.classList.remove("oneTextWhite"))
+    // Remove Theme Two
+    main.classList.remove("twoBgGray")
+    output.classList.remove("twoBgLightGray")
+    buttonGroup.classList.remove("twoBgRedish")
+    primaryButtons.forEach(pb => pb.classList.remove("twoKeysLtYellow", "twoTextDarkGr", "twoKeysDarkGrayOrg"))
+    secondaryButtons.forEach(sb => sb.classList.remove("twoKeysModCyan", "twoTextWhite" , "twoKeysDarkCyan"))
+    equals.classList.remove("twoKeysOrange", "twoKeysDarkOrg", "twoTextWhite")
+    textA.forEach(text => text.classList.remove("twoTextDarkGr"))
+
+    // Add New Theme
+    main.classList.add("threeVioletMain");
+    output.classList.add("threeVioletToggle")
+    buttonGroup.classList.add("threeVioletToggle")
+        // group classes
+    primaryButtons.forEach(pb => pb.classList.add("threeDarkVioletKey", "threeTextYel", "threeMagentaShadow"));
+    secondaryButtons.forEach(sb => sb.classList.add("threeVioletKey", "threeTextWhite" , "threeMagentaShadow"));
+    equals.classList.add("threeCyanKey", "threeCyanShadow", "threeTextBlue")
+    textA.forEach(text => text.classList.add("threeTextYel"))
+}
 
 (() => {
   themeOne()
 })();
-
-function themeOne() {
-    main.classList.add("oneBg");
-    output.classList.add("oneScreen")
-    buttonGroup.classList.add("oneScreen")
-    butt.classList.add("oneKeyGrOrange")
-    equals.classList.add("oneKeyBgRed", "oneKeyShadowRed")
-
-}
-
-
-
-function themeTwo() {
-    buttonGroup.classList.remove("purple", "orange");
-    buttonGroup.classList.add("black");
-}
-
-function themeThree() {
-    buttonGroup.classList.remove("purple", "black");
-    buttonGroup.classList.add("orange");
-}
-
-// radios.forEach((radio) => {
-//     radio.addEventListener("click", () => {
-//         return(radio.id === 1 ? buttonGroup.classList.add("purple") : console.log(radio.id))
-//     })
-// })
 
 // Calc Functionality
 
